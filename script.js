fe7f56c1f7ba498b17e614c88987a8299e4cb7a0
Marcos Azuma <marcos.azuma@hotmail.com>
2025-03-13 10:25:28 -0300

    add curriculo

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("linkedin").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/marcos-azuma-a4a50028a/", "_blank");
+});
+
+document.getElementById("github").addEventListener("click", function() {
+    window.open("https://github.com/marcosazuma", "_blank");
+});
+
+document.getElementById("email").addEventListener("click", function() {
+    window.location.href = "mailto:marcos.azuma@hotmail.com";
+});
+
+document.getElementById("phone").addEventListener("click", function() {
+    window.location.href = "tel:+5511965850164";
+});
